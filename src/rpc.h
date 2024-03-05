9c4d144eb5d89276154ec418a819e8631a71686b
liqijing <liqijing19@mails.ucas.ac.cn>
2024-03-05 06:36:26 +0000

    fix DTHUB IP, CMakeLists, Machine IP

--- src/rpc.h
+++ src/rpc.h
@@ -22,8 +22,8 @@
 
 #include <sys/time.h>
 
-#define DTHUB_CLIENT_IP "10.0.200.17"
-#define DTHUB_SERVER_IP "10.0.200.18"
+#define DTHUB_CLIENT_IP "10.0.0.17"
+#define DTHUB_SERVER_IP "10.0.0.18"
 #define TACC_CLIENT_IP "10.0.13.1"
 #define TACC_SERVER_IP "10.0.14.1"
 #define DESKTOP_CLIENT_IP "10.0.2.101"
@@ -33,8 +33,7 @@ static constexpr uint8_t TACC_CLIENT_MAC[6] = {0xb8, 0xce, 0xf6, 0x7f, 0x35, 0x1
 static constexpr uint8_t TACC_SERVER_MAC[6] = {0xb8, 0xce, 0xf6, 0x7f, 0x47, 0xa8,};
 static constexpr uint8_t TACC_Switch_Mac[6] = {0x1c, 0x34, 0xda, 0xf3, 0x9a, 0x48,};
 
-#define MACHINE_IP DESKTOP_CLIENT_IP
-#define MACHINE_MAC TACC_CLIENT_MAC
+#define MACHINE_IP DTHUB_CLIENT_IP
 
 #define DPDK
 #define ZeroCopyTX
